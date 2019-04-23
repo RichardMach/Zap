09e10c1775588f63d261186eed647d667965ec8e
Richard Machado <richardmachado.93@gmail.com>
2019-04-23 12:26:46 -0300

    Layout redefinido para receber login por e-mail; Deleção de códigos que faziam o login por validação de SMS; Criação da Activity Cadastro Usuario;

--- app/src/main/java/com/example/zap/CadastroUsuarioActivity.java
+++ app/src/main/java/com/example/zap/CadastroUsuarioActivity.java
@@ -3,11 +3,11 @@ package com.example.zap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ValidadorActivity extends AppCompatActivity {
+public class CadastroUsuarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_validador);
+        setContentView(R.layout.activity_cadastro_usuario);
     }
 }
